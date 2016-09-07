12cda74095278b8dbd7ef34d4179700aae53cddb
himanshu <himanshusharmahs974@gmail.com>
2016-09-07 20:13:50 +0530

    java nullpointer exception

--- null.java
+++ null.java
@@ -1,15 +1,13 @@
-public class null{
+public class null1{
 
     public static void main(String[] args) {
         try {
-            doSomething();
+            String str=null;
+            System.out.println(str.length());
         } catch (NullPointerException e) {
             System.out.print("Caught the NullPointerException");
         }
     }
 
-    public static void doSomething() {
-        String nullString = null;
-        nullString.endsWith("test");
-    }
+   
 }
